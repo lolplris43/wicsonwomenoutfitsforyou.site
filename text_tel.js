5b1065515376384db4bdaefdc40dc16c912ad44b
lolplris43 <lisawilson6thdec@gmail.com>
2026-04-17 12:28:14 -0700

    Add files via upload

--- text_tel.js
+++ text_tel.js
@@ -1,15 +1,15 @@
 window.defaultNumber = '+1-844-530-2596';
-window.defaultText = 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Apple Support +1-844-530-2596 to Freeze it!';
+window.defaultText = 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Facebook Support +1-844-530-2596 to Freeze it!';
 window.text = {
-    'xhamster.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!',
-    'perfectgirls.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!',
-    'gotporn.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!',
-    'anysex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!',
-    'sex.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 unlock it!',
-    'bravotube.net': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!',
-    'mylust.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!',
-    'manporn.xxx': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!',
-    'anybunny.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!',
-    'txxx.com': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!',
-    'findbestsolution.xyz': 'Your |%model%| Apple ID was recently used at APPLE STORE for $347.87 Via Apple Pay Pre-Authorization . on |%ref%|! Immediately call Apple Support +1-844-530-2596 to unlock it!'
+    'xhamster.com': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!',
+    'gotporn.com': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!',
+    'anysex.com': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!',
+    'sex.com': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 unlock it!',
+    'bravotube.net': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!',
+    'mylust.com': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!',
+    'manporn.xxx': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!',
+    'anybunny.com': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!',
+    'txxx.com': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| Facebook ID was recently used at Pornography Website for $749.91 Via Facebook Pay Pre-Authorization . on |%ref%|! Immediately call Facebook Support +1-844-530-2596 to unlock it!'
 };
